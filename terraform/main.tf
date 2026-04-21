2f3f3641004c2c097321718b3c538060ecc52f6e
Emilien.B <emilbus@outlook.fr>
2026-04-20 22:42:34 -0400

    fix: bucket creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,16 +27,7 @@ resource "google_project_iam_member" "sa_terraform-vm_iam" {
     member = "serviceAccount:${google_service_account.sa_terraform-vm.email}"
 }
 
-resource "google_storage_bucket" "tfstate-bucket" {
-    name = "tfstate-authentik"
-    location = var.region
-    public_access_prevention = "enforced"
-    uniform_bucket_level_access = true
-    force_destroy = false
-    versioning {
-        enabled = true
-    }
-}
+
 
 resource "google_compute_instance" "vm-terraform" {
     name = "vm-terraform2"
