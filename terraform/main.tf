14935556eed8cda07afb63055e40d8c8187e2c69
Emilien.B <emilbus@outlook.fr>
2026-04-20 23:08:01 -0400

    Install modern Docker Compose v2 to fix interpolation error

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "google_project_iam_member" "sa_terraform-vm_iam" {
 
 
 resource "google_compute_instance" "vm-terraform" {
-    name = "vm-terraform4"
+    name = "vm-terraform5"
     machine_type = "e2-medium"
     zone = "northamerica-northeast1-a"
     boot_disk {
